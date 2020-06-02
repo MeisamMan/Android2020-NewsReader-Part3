e1a52655c6c7db6ffccc3d07b4ad0c5a8a028804
Meisam <Meisam@meiCode.org>
2020-06-02 12:58:34 +0400

    The source code for the video called 'NewsReader Application (Part 3)' in the Android App Development Master Class for 2020

---
+++ app/src/main/java/org/meicode/newsreader/WebsiteActivity.java
@@ -0,0 +1,39 @@
+package org.meicode.newsreader;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class WebsiteActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_website);
+
+        Intent intent = getIntent();
+        if (null != intent) {
+            String url = intent.getStringExtra("url");
+            if (null != url) {
+                webView = findViewById(R.id.webView);
+                webView.setWebViewClient(new WebViewClient());
+                webView.getSettings().setJavaScriptEnabled(true);
+                webView.loadUrl(url);
+            }
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            getObbDir();
+        }else {
+            super.onBackPressed();
+        }
+    }
+}
